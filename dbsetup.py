46803ad994f9643bd585a096532535b435c18952
Madison-Bold <maddiebold@gmail.com>
2018-01-31 02:13:51 -0500

    saved db file

--- dbsetup.py
+++ dbsetup.py
@@ -16,6 +16,7 @@ class User(Base):
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
 	email = Column(String(250), nullable=False)
+	picture = Column(String)
 
 class Luthier(Base):
 	__tablename__ = 'luthier'
