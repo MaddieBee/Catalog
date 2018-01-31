69f383650b1a996b96143daf73a51919199896d6
Madison-Bold <maddiebold@gmail.com>
2018-01-31 02:43:24 -0500

    getting server up and working

--- application.py
+++ application.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request, redirect, jsonify, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Luthier, Base, CelloItem
+from db_setup import Luthier, Base, CelloItem
 
 app = Flask(__name__)
-
-
-
-
-
 
 engine = create_engine('sqlite:///cellocatalog.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+# Show all Luthiers
+@app.route('/')
+@app.route('/luthier/')
+def showLuthiers():
+    luthiers = session.query(Luthier).all()
+    # return "This page will show all my restaurants"
+    return render_template('luthiers.html', luthiers=luthiers)
 
 
 
