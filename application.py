ed7614004025315ab25e94a4732cc01466d0770c
Madison-Bold <maddiebold@gmail.com>
2018-03-27 02:41:33 -0400

    Added Oauth and Client Secret Key

--- application.py
+++ application.py
@@ -7,8 +7,19 @@ from sqlalchemy.orm import sessionmaker
 from database_setup import Luthier, Base, Cello, User
 
 from flask import session as login_session 
+import random, string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests 
 
 app = Flask(__name__)
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Application"
 
 engine = create_engine('sqlite:///cellocatalog.db')
 Base.metadata.bind = engine
@@ -18,6 +29,15 @@ session = DBSession()
 
 
 # Main Page.  Displays all Luthier's cellos.  WELL????
+
+
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in range(32))
+    login_session['state'] = state
+    return "The current session state is %s" % login_session['state']
+
 
 @app.route('/')
 @app.route('/index', methods=['GET'])
